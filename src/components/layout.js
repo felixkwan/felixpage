19a399c6cdb43b46d935f1b9dd00243ea8320d42
felixkwan <felixkwan101@gmail.com>
2020-12-31 15:44:21 +0800

    environment update

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,15 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
